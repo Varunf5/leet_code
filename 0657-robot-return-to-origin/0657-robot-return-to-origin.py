f145bb54ee685bc6916edc2fe1b149d2b3454a1c
Varunf5 <153079294+Varunf5@users.noreply.github.com>
2026-04-05 15:30:39 +0530

    Time: 17 ms (30.46%), Space: 19.3 MB (67.4%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,15 +1,11 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        moves=moves.strip()
         x=y=0
         for i in moves:
-            if i not in "UDLR":
-                return False
             if i=="R":x+=1
             elif i=="L":x-=1
             elif i=="D": y-=1
             elif i=="U" : y+=1
-            else: continue
         return x==0 and y==0
 
         
