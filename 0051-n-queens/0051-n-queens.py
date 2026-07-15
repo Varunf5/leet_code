6071b7f9e83a25bf6b7fd892827a0f2f1b7dbf72
Varunf5 <153079294+Varunf5@users.noreply.github.com>
2026-07-15 16:31:58 +0530

    Time: 16 ms (29.53%), Space: 19.6 MB (68.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,19 +1,26 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col,tl,tr=[0]*n,[0]*(2*n+1),[0]*(2*n+1)
-        b=[['.']*n for i in range(n)]
-        res=[]
-        def nqueen(i,n):
-            if i==n:
-                t=[]
-                for i in range(n):
-                    t.append(''.join(b[i]))
-                res.append(t)
-                return
+        r=[]
+        rows,cols,posd,negd=set(),set(),set(),set()
+        l=[["."]*(n) for p in range(n)]
+        def bt(i):
+            if(i==n):
+                d=["".join(p) for p in l]
+                r.append(d)
             for j in range(n):
-                if not col[j] and not tl[i-j+n-1] and not tr[i+j]:
-                    col[j]=tl[i-j+n-1]=tr[i+j]=1;b[i][j]="Q"
-                    nqueen(i+1,n)
-                    b[i][j]='.';col[j]=tl[i-j+n-1]=tr[i+j]=0
-        nqueen(0,n)
-        return res        
+                if i in rows or j in cols or i+j in posd or i-j in negd or l[i][j] =="Q":
+                    continue
+                l[i][j]="Q"
+                rows.add(i)
+                cols.add(j)
+                posd.add(i+j)
+                negd.add(i-j)
+                bt(i+1)
+                l[i][j]="."
+                rows.discard(i)
+                cols.discard(j)
+                posd.discard(i+j)
+                negd.discard(i-j)
+        bt(0)
+        return r
+
